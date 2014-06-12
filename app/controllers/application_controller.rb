7a85c5fc07ce1d598559c5fe8f66a144869da458
Justin Weinzimmer <justinweinzimmer@yahoo.com>
2014-06-12 14:47:47 -0400

    new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 def current_cart
+  if Cart.count == 0
+    @cart = Cart.new
+    @cart.save!
+    session[:cart_id] = @cart.id
+  end
   if session[:cart_id]
     @current_cart ||= Cart.find(session[:cart_id])
     session[:cart_id] = nil if @current_cart.purchased_at
